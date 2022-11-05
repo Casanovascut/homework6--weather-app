b527a37b04cde8ab9b005418a5d63fd0711fbb39
Sebastian <smcasanova17@hotmail.com>
2022-11-04 21:42:52 -0400

    first commit

--- Assets/script.js
+++ Assets/script.js
@@ -3,8 +3,9 @@ var wthrApiKey = '928a790dea4c4ffa1546ae2e3cf64aef'
 // make button show weather results for specific cities
 $('#btn').on('click',()=>{
     let cityFinder = $('#inputBox').val()
-    $.get("https://api.openweathermap.org/data/2.5/forecast?q="+cityFinder+"&appid="+wthrApiKey,function
+    $.get("https://api.openweathermap.org/data/2.5/forecast/?q="+cityFinder+"&appid="+wthrApiKey,function
     (data){
+        
         console.log("data received:", data) 
         console.log(data.list[0].main.temp)  
     })
