2e0ef18f6c2bf424d9e3a79458a8eb8f663c429f
Sebastian <smcasanova17@hotmail.com>
2022-11-05 09:06:54 -0400

    trying to create card elements for each weather day

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,25 @@
 var wthrApiKey = '928a790dea4c4ffa1546ae2e3cf64aef'
-
+var cardContainerEl = document.getElementById('cardcontainer')
 // make button show weather results for specific cities
 $('#btn').on('click',()=>{
     let cityFinder = $('#inputBox').val()
     $.get("https://api.openweathermap.org/data/2.5/forecast/?q="+cityFinder+"&appid="+wthrApiKey,function
     (data){
-        
         console.log("data received:", data) 
-        console.log(data.list[0].main.temp)  
-    })
+       // return 5 day weather data
+        for(var i = 0;i<5;i++){
+        //create card containers and put them in the card container element
+            var wthrCardEl = document.createElement('div')
+            wthrCardEl.classList.add('card')
+            var cardBdy = document.createElement('div')
+            cardBdy.classList.add('card-body')
+            var crdtxt = document.createElement('p')
+            crdtxt.classList.add('card-text')
+            cardContainerEl.appendChild(wthrCardEl)
+            wthrCardEl.appendChild(cardBdy)
+            cardBdy.appendChild(crdtxt)
+            crdtxt.textContent = data.list[i].main.temp
+    }})
     
 })
 
